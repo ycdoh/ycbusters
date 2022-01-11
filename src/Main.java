00542956daed4ef744d2cad6c9fb3db6d819438c
ycdoh1 <ycdoh1@worksmobile.com>
2022-01-11 22:58:45 +0900

    2차시 자료형

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,58 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
-    //기본적인 문법
-    //항상 세미콜론 ; 으로 마침표 찍는다
-    //주석 작성하는법 //이후에 작성되는 문장은 코드로써 인식되지않는다 지금 내가 쓰고있는것도 주석~ (코드를 부연설명할때 씀)
-    /*
-    이런식으로
-    여러 라인을
-    한꺼번에
-    주석처리
-    하는 방법도
-    있다.
-     */
 
-
-    //컴퓨터 = 일하는 사람 + 책상 + 책장
-
-    //책상에 내가 필요한것들은 펼쳐놓는다
-    //기본자료형
-    //정수
-    static int number = 0;
-    //소수
-    static double decimal = 0.1;
-    //문자
-    static char singleword = 'a';
-
-    //객체형
-    //문자열 (이친구는 기본자료형은 아니고 객체(나중에 설명하게될 날이오겠져..)이지만 원체 많이쓰기에
-    static String sentence = "youchul";
 
     public static void main(String arg[]) {
-        //사람한테 일을 시켜볼려면
+        //문자
+        //0 or 1 참거짓
+        //숫자 0123445667 7.2 7.34 1.21312 -1
 
-        //콘솔에 출력하는 method System.out.println();
-        System.out.println(number);
-        //number에 1을 더해서 number라는공간에 그값을 지정 (절대로 같다는 의미가 아님)
-        number = number + 1;
+        //character 약자
+        //(data모양) 이름 = (생긴모양세);
+        //이거는 이러이러한 의도를 갖고 한거고 요거는 요거다
+        char word = 'a';
+        char firstName = 'l';
+        char middleName = 's';
+        char lastName = 'i';
 
-        System.out.println(number);
+        boolean logic = false;
+        //참거짓이 어디에 쓰이냐면
+        //2는 4보다 작다는 true 이 true를 trueOrFalse라는 이름을 갖는 boolean에다가 지정
+        boolean trueOrFalse = 2 < 4; // boolean trueOrFalse = true;
+
+        // = 의 의미 (우선 수학적인것처럼 절대 '같다'는 아님
+        //programming에서 =은 거의 대부분 (우선java에서는 100%) 넣는다라는 의미가 더 맞음;
+        int number = 100;
+        //이게 무슨의미냐면 number라는 공간에 100을 넣는다
+
+        //정수만
+        int numberA = 2;
+        if (numberA < 4) {
+            System.out.println("이새임");
+        }
+
+
+        //소수만
+        double numberB = 2;
+
+        //문자열
+        String name = "이새임";
+        String single = "이";
+
+        //빨간줄로 틀린거 알려줄때 alt + enter 누르면 수정방법을 추천해준다. (다만 영어로)
+        String sdf = "a";
+
+
+        //여기서 출력하겠다
+        System.out.println(word);
+        System.out.println(logic);
+        System.out.println(numberA);
+        System.out.println(numberB);
+        System.out.println(name);
+
+
+        //numberA = 2
+        //numberB = 2.000000
 
 
 
