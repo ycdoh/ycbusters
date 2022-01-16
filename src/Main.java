4cf8eae86c6ad1ecae1c01a44a15e8188a1d849a
ycdoh1 <ycdoh1@worksmobile.com>
2022-01-16 22:23:46 +0900

    3차시 비교연산자, 조건문, 반복문

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 
@@ -53,6 +56,100 @@ public class Main {
 
         //numberA = 2
         //numberB = 2.000000
+
+
+        //비교연산
+//        int result = 2*(4+67);
+
+        boolean r = 2 >= 4;
+
+
+        // >
+        // <
+        // <=
+        // >=
+        // ==
+        // !=
+
+        boolean r2 = 2!=2;
+        boolean r3 = 2!=3;
+
+        System.out.println(r2);
+        System.out.println(r3);
+
+
+
+        int a = 100;
+        int b = 200;
+        int x = 400;
+        //조건문
+        if (x != (a + b)) {
+            System.out.println("결과값 참");
+        } else {
+            System.out.println("거짓");
+        }
+
+
+        if (x == 100) {
+            System.out.println("1");
+        } else if (x == 200) {
+            System.out.println("2");
+        } else if (x == 300) {
+            System.out.println("3");
+        }
+
+
+        int c = 10;
+
+        //c가 50보다 클경우 "50"를 출력하고
+        //c가 150보다 클경우 "150을 출력하고
+        //그외에는 "결과없음"을 출력하는 조건문을 작성하세요
+
+        if (c>50) {
+            System.out.println("50");
+        } else if (c>150) {
+            System.out.println("150");
+        } else {
+            System.out.println("결과없음");
+        }
+
+        //반복문
+        //for 반복
+        // i = i + 1
+        //for (시작할인덱스; 인덱스조건 (반복할 조건); 1cycle이후에 할 작업)
+        for (int i=0; i < 100; i= i+1) {
+            //do something
+//            System.out.println("I did something");
+//            System.out.println(i);
+        }
+
+        //while반복
+        // while (반복조건)
+        int i=0;
+        while (i < 100) {
+//            System.out.println("I did something");
+//            System.out.println(i);
+            i = i+1;
+        }
+
+        //for문으로 짜세요
+        //0부터 99까지반복하는데
+        //출력은 짝수만해보기 (hint 짝수는 %2 연산하면 나머지가 0이다)
+        for (int j=0; j < 100; j=j+1) {
+            if (j%2==0) {
+                System.out.println(j);
+            }
+        }
+
+        //while로 짜보세요
+        //result에 1부터 100까지의 모든 정수의 합을 구해서 넣어보십쇼
+
+        int result = 0;
+        int z = 1;
+        while (z < 101) {
+            result = result + z;
+            z = z+1;
+        }
 
 
 
